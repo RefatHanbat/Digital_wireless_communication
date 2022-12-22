9a63cdf16eb17063d82460edba8d26a0bf08e44d
RefatHanbat <121217767+RefatHanbat@users.noreply.github.com>
2022-12-22 21:12:51 +0900

    rayleigh and additive white gaussian noise

--- sequenceingBitAndChannelEncoding.m
+++ sequenceingBitAndChannelEncoding.m
@@ -29,3 +29,33 @@ for i=1:length(modbits)
     mod_linear_4qam(:,i)=qammod(bi_linear(i,:), 4);
 end
 
+%%channel model
+%1)AWGN
+channelBPSK = comm.AWGNChannel('EbNo',15,'BitsPerSymbol',1); % define a channel model
+channel4QAM = comm.AWGNChannel('EbNo',15,'BitsPerSymbol',2);
+awgn_bpsk=channelBPSK(mod_linear_bpsk);
+awgn_4qam = channel4QAM(mod_linear_4qam);
+scatterplot(awgn_bpsk(:,1))
+title('BPSK constellation after AWGN channel')
+grid on;
+% 2) Rayleigh channel model 
+fm=1e6;
+sampling=5*fm;
+rayChan = comm.RayleighChannel(...
+    'SampleRate',sampling, ...
+    'PathDelays',[1e-9 1.5e-5], ...
+    'AveragePathGains',[2 1], ...
+    'NormalizePathGains',true, ...
+    'DopplerSpectrum',{doppler('Gaussian',0.6),doppler('Flat')}, ...
+    'RandomStream','mt19937ar with seed', ...
+    'Seed',22); % creating the object for the Rayleigh channel
+for i=1:length(mod_linear_bpsk)
+    rayChan_bpsk(:,i) = rayChan(mod_linear_bpsk(:,i));
+end
+for i=1:length(mod_linear_4qam)
+    rayChan_4qam(:,i) = rayChan(mod_linear_4qam(:,i));
+end
+scatterplot(rayChan_bpsk(:,1))
+title('BPSK constellation after Releigh channel')
+grid on;
+
