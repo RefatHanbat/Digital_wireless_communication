c1730f39e6fb3860eaeab4b492dedfcef5fc3c8b
RefatHanbat <121217767+RefatHanbat@users.noreply.github.com>
2022-12-26 19:09:04 +0900

    complete of LTE

--- sequenceingBitAndChannelEncoding.m
+++ sequenceingBitAndChannelEncoding.m
@@ -58,4 +58,90 @@ end
 scatterplot(rayChan_bpsk(:,1))
 title('BPSK constellation after Releigh channel')
 grid on;
+%% Receiver Side
+%% Zero forcing Equilizer
+g=[1];
+zf_linear_awgn2=ZF(g,awgn_bpsk);
+zf_linear_awgn4=ZF(g,awgn_4qam);
+zf_linear_rayChan2 = ZF(g,rayChan_bpsk);
+zf_linear_rayChan4 = ZF(g,rayChan_4qam);
+
+%% Demodulation
+
+for i=1:length(zf_linear_awgn2)
+    awgnD2_linear(:,i) = comm.BPSKDemodulator(zf_linear_awgn2(:,i)).';
+    awgnD4_linear(:,i) = qamdemod(zf_linear_awgn4(:,i),4);
+    rayChan2_linear(:,i) = H2(zf_linear_rayChan2(:,i)).';
+    rayChan4_linear(:,i) = qamdemod(zf_linear_rayChan4(:,i),4);
+end
+
+function Xh = ZF(h,r)
+
+%r --- signal at the receiver
+% h--- impulse response of the channel 
+%Computing inverse impulse response
+
+gD=tf(h,1); % Gain of the channel
+
+f=1/gD; % taking inverse of the channel gain
+
+[num,den]=tfdata(f,'v'); % extracting numerator and denominator coefficients 
+
+%Zero forcing
+Xh=filter(num,den,r); % filtering 
+
+% Xh=Xh(1:end); %this done for technical reasons 
+end
+
+
+%H2 = comm.BPSKDemodulator;
+%% FEC Decoder
+%Linear Decoder
+for i=1:length(awgnD2_linear)
+    awgn_linear_bpsk(:,i) = decode(awgnD2_linear(:,i),n,k);
+    awgn_linear_4(:,i) = decode(awgnD4_linear(:,i),n,k);
+    rayChan_linear_bpsk(:,i) = decode(rayChan2_linear(:,i),n,k);
+    rayChan_linear_4(:,i) = decode(rayChan4_linear(:,i),n,k);
+end
+%% Deinterleaver
+% first take the transpose 
+awgn_linear_bpsk=awgn_linear_bpsk';
+awgn_linear_4=awgn_linear_4';
+rayChan_linear_bpsk=rayChan_linear_bpsk';
+rayChan_linear_4=rayChan_linear_4';
+% Block Deinterleaving
+awgnlinear_bpsk = matdeintrlv(awgn_linear_bpsk, 32, 32);
+awgnlinear_4 = matdeintrlv(awgn_linear_4, 32, 32);
+rayChanlinear_bpsk = matdeintrlv(rayChan_linear_bpsk, 32, 32);
+rayChanlinear_4 = matdeintrlv(rayChan_linear_4, 32, 32);
+%% BER Calculation
+% Linear AWGN
+% BER Plots
+EbNo = 1:1:12;
+M1=2; % symbols in BPSK
+M2=4; % symbols in 4QAM
+den=1;
+num1 = ones(2,1)/2; % for BPSK
+num2 = ones(4,1)/4; % for 4QAM
+Nsamp=100; % number of samples to be considered
+TX_bpsk=mod_linear_bpsk(:);
+TX_4qam=mod_linear_4qam(:);
+RX_bpsk_awgn=zf_linear_awgn2(:);
+RX_4qam_awgn=zf_linear_awgn4(:);
+RX_bpsk_raychan=zf_linear_rayChan2(:);
+RX_4qam_raychan=zf_linear_rayChan2(:);
+ber_linear_AWGN_2 = semianalytic(TX_bpsk,RX_bpsk_awgn,'psk/nondiff',M1,Nsamp,num1,den,EbNo);
+ber_linear_AWGN_4 = semianalytic(TX_4qam,RX_4qam_awgn,'psk/nondiff',M2,Nsamp,num2,den,EbNo);
+ber_linear_raychan_2 = semianalytic(TX_bpsk,RX_bpsk_raychan,'psk/nondiff',M1,Nsamp,num1,den,EbNo);
+ber_linear_raychan_4 = semianalytic(TX_4qam,RX_4qam_raychan,'psk/nondiff',M2,Nsamp,num2,den,EbNo);
+figure(1)
+semilogy(EbNo,ber_linear_AWGN_2);
+hold on
+semilogy(EbNo,ber_linear_AWGN_4);
+semilogy(EbNo,ber_linear_raychan_2);
+semilogy(EbNo,ber_linear_raychan_4);
+grid on
+xlabel('EbNo');
+ylabel('BER');
+legend('AWGN 4QAM','AWGN BPSK','Ray 4QAM','Ray BPSK')
 
